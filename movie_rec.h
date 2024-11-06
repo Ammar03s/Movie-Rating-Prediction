665b490208d65ed6147b363b90f27948656dbc04
Ammar <ammarsalaheye@gmail.com>
2024-11-07 00:17:40 +0300

    Update movie_rec.h

--- movie_rec.h
+++ movie_rec.h
@@ -14,20 +14,20 @@ class RecommenderSystem {
 public:
     RecommenderSystem();
 
-    // User-related functions
+    // User related func
     void addUserInfo(int id, int item_id, double rating);
     void addtestInfo(int id, int item_id);
 
-    // Prediction functions
+    // Prediction func
     double IBCF(int item_id, int user_id);
     list<double> make_predictions();
 
-    // Similarity and error functions
+    // Similarity/ error func
     double rmse(const list<double> &predictions, const list<double> &true_values);
     double cosine_similarity(const vector<double> &vec1, const vector<double> &vec2);
     double item_similarity(map<int, double> v1, map<int, double> v2);
 
-    // Utility functions
+    //the Utility func
     void printUser(int id);
     void printAllUsers();
     void printTestList();
@@ -35,14 +35,14 @@ public:
     void topUsers();
     void topItems();
     
-    // Sorting helper
+    // Sorting helper ig
     void sortArray(int(&rate)[10], int(&id)[10]);
 
 private:
     map<int, map<int, double>> userMap;
     list<pair<int, int>> testList;
 
-    // Find minimum index in an array (template function must be defined in header)
+    // Find min. index in an array (check header)
     template<class type>
     int findmin_index(type array[], int size) {
         type min = array[0];
